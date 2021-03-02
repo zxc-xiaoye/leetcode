9ecca797ea5da8556c7eaa45d68b656d86fcc64c
xiaoye <543335448@qq.com>
2021-03-02 21:04:39 +0800

    leetcode_617

---
+++ src/com/zxc/leetcode/mergetwobinarytrees/Main.java
@@ -0,0 +1,8 @@
+package com.zxc.leetcode.mergetwobinarytrees;
+
+/**
+ * @author xiaoye
+ * @create 2021-03-02 20:55
+ */
+public class Main {
+}
---
+++ src/com/zxc/leetcode/mergetwobinarytrees/Solution.java
@@ -0,0 +1,24 @@
+package com.zxc.leetcode.mergetwobinarytrees;
+
+/**
+ * @author xiaoye
+ * @create 2021-03-02 20:55
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val = root1.val + root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+
+        return root1;
+
+    }
+}
