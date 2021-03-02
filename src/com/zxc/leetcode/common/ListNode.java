46d0786d95fdc65d64e5ffedabbd688b635ff2a6
xiaoye <543335448@qq.com>
2021-03-02 21:28:08 +0800

    commit common

---
+++ src/com/zxc/leetcode/common/ListNode.java
@@ -0,0 +1,13 @@
+package com.zxc.leetcode.common;
+
+/**
+ * @author xiaoye
+ * @create 2021-03-02 21:25
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/com/zxc/leetcode/common/TreeNode.java
@@ -0,0 +1,18 @@
+package com.zxc.leetcode.common;
+
+/**
+ * @author xiaoye
+ * @create 2021-03-02 21:26
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+}
